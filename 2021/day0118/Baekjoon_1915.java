a48e6ca10188f8ef0a0eab07e3b37a657bda408b
winmo95 <seungmo6722@gmail.com>
2021-01-18 10:36:01 +0900

    백준 1915- 정사각형 : DP

--- 2021/day0118/Baekjoon_1915.java
+++ 2021/day0118/Baekjoon_1915.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class swea_1915 {
+public class Baekjoon_1915 {
 	static int n,m,map[][], dp[][];
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
